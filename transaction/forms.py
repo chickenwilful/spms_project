ca9620c0bd8cd65d5e147c830cd17f1dda34047b
chickenwilful <hoangyenng060493@gmail.com>
2015-02-04 12:46:05 +0800

    Update filter form

--- transaction/forms.py
+++ transaction/forms.py
@@ -5,9 +5,13 @@ HOUSE_TYPE_CHOICES = (
     ('h', 'HDB'),
     ('c', 'Condo'),
 )
+
+ROOM_CHOICES = [('', 'All')] + [(i, i) for i in range(1, 9)]
+
+
 class FilterForm(forms.Form):
     type = forms.ChoiceField(label="Type", choices=HOUSE_TYPE_CHOICES)
     name = forms.CharField(label="HouseName", widget=forms.TextInput(), required=False)
     postal_code = forms.CharField(label="PostalCode", widget=forms.TextInput(), required=False)
     address = forms.CharField(label="Address", widget=forms.TextInput(), required=False)
-    room_count = forms.IntegerField(label="No.Bedroom", widget = forms.TextInput(), required=False)
+    room_count = forms.ChoiceField(label="No.Bedroom", choices=ROOM_CHOICES, required=False)
